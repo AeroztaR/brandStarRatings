e7ef033b1b9386ab2fc4041d51d0c2e6c9a7acea
AeroztaR <jorguinhos89@gmail.com>
2018-10-13 05:29:57 +0500

    add styles and base js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 'use strict';
 
-let doc = document;   
+let doc = document,
+    // init ratings
+    ratings = {
+        Adidas: 4.7,
+        Nike: 4.9,
+        Munich: 4.5,
+        Mizuno: 4,
+        Joma: 3.2,
+        Umbro: 4.3
+    },
+    // total stars
+    starsTotal = 5;
 
+    // run on DOM loads
+    doc.addEventListener('DOMContentLoaded', getRatings);
+
+    // get ratings
+    function getRatings() {
+        for(let rating in ratings) {
+            let starPercentage = (ratings[rating] / starsTotal) * 100;
+            
+            // round to 10
+            let starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+            
+            // width stars-inner percentage
+            doc.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+            
+        }
+    }
 
 
